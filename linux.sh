1fcc491c2621195bd9b20a20b0d0faf602638a9c
Wang Hao <991060@gmail.com>
2023-05-24 20:41:20 +0800

    Fix Linux local build

--- linux.sh
+++ linux.sh
@@ -1,9 +1,10 @@
-[ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
-WORKSPACE=$GITHUB_WORKSPACE
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORKSPACE=$CURR_DIR
 HOMEPATH=~
 VERSION=16.16.0
 
 echo "=====[Patching Node.js]====="
+cd $WORKSPACE/node
 node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/lib_uv_add_on_watcher_queue_updated_v$VERSION.patch
 node $WORKSPACE/node-script/add_arraybuffer_new_without_stl.js deps/v8
 node $WORKSPACE/node-script/make_v8_inspector_export.js
@@ -24,3 +25,5 @@ cp -r deps/v8/include ../puerts-node/nodejs/deps/v8
 
 mkdir -p ../puerts-node/nodejs/lib/Linux/
 cp out/Release/libnode.so.* ../puerts-node/nodejs/lib/Linux/
+
+git reset --hard
